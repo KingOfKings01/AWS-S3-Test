db7e3d20612749d175cd8d1a661f2448a7f064db
KingOfKings01 <aasifkhan9605@gmail.com>
2024-08-13 07:08:23 +0530

    Put function is ready.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import "dotenv/config";
@@ -19,17 +19,28 @@ async function getObjectURL(key) {
 
   const url = await getSignedUrl(s3Client, command)
   //Todo: If you want url get expires in 5 seconds
-//   const url = await getSignedUrl(s3Client, command, { expiresIn: 5 }); 
+//   const url = await getSignedUrl(s3Client, command, { expiresIn: 5 });  
   return url;
 }
 
+async function putObject(filename, contentType){
+    const command = new PutObjectCommand({
+        Bucket: process.env.BUCKET_NAME,    
+        Key: `upload-object/${filename}`,
+        ContentType: contentType
+    })
+    console.log("filename", filename);
+    console.log("contentType", contentType);
+    const url = await getSignedUrl(s3Client, command,)
+    return url;
+}
+
 async function main() {
-  console.log(
-    "Url for my file: ",
-    await getObjectURL("pexels-iriser-1408221.jpg")
-  );
+//   console.log(
+//     "Url for my file: ",
+//     await getObjectURL("pexels-iriser-1408221.jpg")
+//   );
+console.log("URL for uploaded file: ", await putObject(`image-${Date.now()}.jpg`, "image/jpg"));
 }
 
 main();
-
-// console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.BUCKET_NAME);
